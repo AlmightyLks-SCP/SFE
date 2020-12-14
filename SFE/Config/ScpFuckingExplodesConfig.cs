cf1b096fc4f29179924b6a5469036ee810797307
almightylks <lukasbarber@web.de>
2020-12-14 23:15:53 +0100

    init

---
+++ SFE/Config/ScpFuckingExplodesConfig.cs
@@ -0,0 +1,12 @@
+﻿using Synapse.Config;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace SFE.Config
+{
+    public class ScpFuckingExplodesConfig : AbstractConfigSection
+    {
+        [Description("IDs of SCPs which shall explode on death")]
+        public List<int> ExplodingScps { get; set; } = new List<int>();
+    }
+}
---
+++ SFE/SFE.cs
@@ -0,0 +1,37 @@
+﻿using SFE.Config;
+using Synapse.Api;
+using Synapse.Api.Events.SynapseEventArguments;
+using Synapse.Api.Plugin;
+
+namespace SFE
+{
+    [PluginInformation(
+        Author = "AlmightyLks",
+        Description = "Make SCPs go poof when ded",
+        Name = "SCP Fucking Explodes",
+        SynapseMajor = 2,
+        SynapseMinor = 2,
+        SynapsePatch = 0,
+        Version = "1.0.0"
+        )]
+    public class SFE : AbstractPlugin
+    {
+        [Synapse.Api.Plugin.Config(section = "SCP Fucking Explodes")]
+        public ScpFuckingExplodesConfig Config { get; set; }
+
+        public override void Load()
+        {
+            Synapse.Api.Events.EventHandler.Get.Player.PlayerDeathEvent += Player_PlayerDeathEvent;
+
+            base.Load();
+        }
+
+        private void Player_PlayerDeathEvent(PlayerDeathEventArgs ev)
+        {
+            if (!Config.ExplodingScps.Contains(ev.Victim.RoleID))
+                return;
+
+            Map.Get.Explode(ev.Victim.Position, Synapse.Api.Enum.GrenadeType.Grenade, ev.Victim);
+        }
+    }
+}
